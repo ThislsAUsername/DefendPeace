e180ad4fa9bc4c5d31d8de421097c656f3855d59
ThatIsAUsername <ThatIsAUsername@users.noreply.github.com>
2015-12-26 01:44:26 -0600

    Added colors for all of th terrain types.

--- src/UI/MapView.java
+++ src/UI/MapView.java
@@ -54,8 +54,34 @@ public class MapView extends javax.swing.JPanel {
 			tileColor = new Color(186,255,124);
 			break;
 		case CITY:
+			tileColor = Color.GRAY;
+			break;
+		case FACTORY:
+			tileColor = Color.DARK_GRAY; // TODO: Make this team color, if owned.
+			break;
+		case FOREST:
+			tileColor = Color.GREEN;
+			break;
+		case WATER:
+			tileColor = Color.BLUE;
+			break;
+		case HQ:
+			tileColor = Color.RED; // TODO: Make this team color.
+			break;
+		case MOUNTAIN:
+			tileColor = new Color(101,40,26);
+			break;
+		case REEF:
+			tileColor = new Color(212,144,56);
+			break;
+		case ROAD:
 			tileColor = Color.LIGHT_GRAY;
 			break;
+		case SHOAL:
+			tileColor = new Color(240,209,77);
+			break;
+			default:
+				tileColor = Color.BLACK;
 		}
 		
 		g.setColor(tileColor);
