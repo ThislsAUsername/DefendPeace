0f0d3ed94c3bbf7704facb180d0a12d6f151e1e8
ThatIsAUsername <ThatIsAUsername@users.noreply.github.com>
2015-12-26 22:37:50 -0600

    Added a bunch of input logic to MapController, plus the GameMenu class. Infantry can be produced, but that's it so far; not even draw support.

---
+++ src/UI/GameMenu.java
@@ -0,0 +1,39 @@
+package UI;
+
+import Engine.MapController;
+
+public class GameMenu {
+	
+	private Enum options[];
+	int selectedOption = 0;
+	
+	public GameMenu(Enum[] options)
+	{
+		this.options = options;
+	}
+
+	public void handleMenuInput(InputHandler.InputAction action)
+	{
+		switch(action)
+		{
+		case UP:
+			selectedOption = (selectedOption > 0)? selectedOption-- : selectedOption;
+			break;
+		case DOWN:
+			selectedOption = (selectedOption < options.length-1)? selectedOption++ : selectedOption;
+			break;
+		case LEFT:
+		case RIGHT:
+		case ENTER:
+		case BACK:
+		case NO_ACTION:
+			default:
+				System.out.println("WARNING! gameMenu.handleMenuInput() was given invalid action enum (" + action + ")");
+		}
+	}
+	
+	public Enum getSelectedAction()
+	{
+		return options[selectedOption];
+	}
+}
