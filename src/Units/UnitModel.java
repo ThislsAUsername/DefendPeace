e26e1271492e59c3abe4d096b75ce581e60ed175
ThatIsAUsername <ThatIsAUsername@users.noreply.github.com>
2015-12-30 23:10:36 -0600

    Reorganized member varible position for consistency.

--- src/Units/UnitModel.java
+++ src/Units/UnitModel.java
@@ -10,32 +10,34 @@ public class UnitModel {
 	
 	public String name;
 	public UnitEnum type;
+	public int moneyCost = 9001;
+	public int maxFuel;
+	public int idleFuelBurn;
+	public int movePower;
+	public MoveType propulsion;
+	public MapController.GameAction[] possibleActions;
+	
 	public double maxHP;
 	public int holdingCapacity;
 	public Vector<UnitEnum> holdables;
 	public double COStr;
 	public double CODef;
-	public MapController.GameAction[] possibleActions;
 
-	public int moneyCost = 9001;
 	public int minRange = 1;
 	public int maxRange = 1;
-	public int maxFuel;
-	public int idleFuelBurn;
-	public int movePower;
-	public MoveType propulsion;
 	
 	public UnitModel(String pName, UnitEnum pType, int cost, int pFuelMax, int pIdleFuelBurn, int pMovePower, MoveType pPropulsion) {
 		name 		 = pName;
 		type 		 = pType;
 		moneyCost	 = cost;
-		maxHP        = 100;
-		COStr        = 100;
-		CODef        = 100;
 		maxFuel		 = pFuelMax;
 		idleFuelBurn = pIdleFuelBurn;
 		movePower    = pMovePower;
 		propulsion   = pPropulsion;
+		
+		maxHP        = 100;
+		COStr        = 100;
+		CODef        = 100;
 		holdingCapacity = 0;
 	}
 }
