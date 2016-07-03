faa1f12f4548f1f67aeaedafae83242dba6414aa
ThatIsAUsername <ThatIsAUsername@users.noreply.github.com>
2016-07-03 01:28:04 -0500

    Added fancy shimmer effect to CO select screen.

--- src/UI/Art/SpriteArtist/SpriteCOSetupArtist.java
+++ src/UI/Art/SpriteArtist/SpriteCOSetupArtist.java
@@ -3,6 +3,7 @@ package UI.Art.SpriteArtist;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Polygon;
 import java.awt.image.BufferedImage;
 
 import CommandingOfficers.CommanderLibrary;
@@ -14,11 +15,23 @@ public class SpriteCOSetupArtist
   private static Dimension dimensions = null;
   private static int drawScale = 2;
 
-  private static final Color BGCOLOR = new Color(234, 243, 255);
+  private static final Color BGCOLOR = new Color(234, 243, 255); // Light blue background.
+  private static final Color BGSHIMMER= new Color(248, 248, 255); // Lighter blue shimmer effect.
   private static final Color MENUFRAMECOLOR = new Color(169, 118, 65);
   private static final Color MENUBGCOLOR = new Color(234, 204, 154);
 
   private static double animHighlightedPlayer = 0;
+
+  // variables for background shimmer effect
+  private static int shimmerStretch = 160*drawScale; // TODO: This should really just be screen height.
+  private static int shimmerThickness = 25*drawScale;
+  private static int shimmerSpacing = 100*drawScale; // Distance between adjacent bands.
+  private static int[] xPoints = {0, shimmerStretch, shimmerStretch+shimmerThickness, shimmerThickness};
+  private static int[] yPoints = {shimmerStretch, 0, 0, shimmerStretch};
+  private static Polygon shimmerPoly = new Polygon(xPoints, yPoints, xPoints.length); // Shimmer shape to draw.
+  private static double shimmerBasePoint = -(shimmerStretch+shimmerSpacing); // Where to draw the first shimmer.
+  private static long lastDrawTime = System.currentTimeMillis(); // Used to control shimmer drift.
+  private static double shimmerPxPerMs = 0.03; // Controls the speed of shimmer movement.
 
   public static void setDimensions(Dimension d)
   {
@@ -31,7 +44,8 @@ public class SpriteCOSetupArtist
     g.setColor(BGCOLOR);
     g.fillRect(0,0,dimensions.width, dimensions.height);
 
-    // TODO: draw fancy background effects.
+    // Draw fancy background effects.
+    drawBGShimmer(g);
 
     /////////////////// MiniMap ////////////////////////
     BufferedImage miniMap = SpriteMiniMapArtist.getMapImage( mapInfo );
@@ -99,7 +113,7 @@ public class SpriteCOSetupArtist
     int drawY = yCenter - ( (portrait.getHeight()*drawScale) / 2);
     int drawW = portrait.getWidth() * drawScale;
     int drawH = portrait.getHeight() * drawScale;
-    
+
     // Draw frame box
     g.setColor( MENUFRAMECOLOR );
     g.fillRect(drawX - (2*drawScale), drawY - (2*drawScale), drawW + (4*drawScale), drawH + (4*drawScale));
@@ -110,5 +124,44 @@ public class SpriteCOSetupArtist
 
     // Draw CO Portrait
     g.drawImage(portrait, drawX, drawY, drawW, drawH, null);
+  }
+
+  /**
+   * This function works by tracking a single point, where the first shimmer begins, and then drawing
+   * each band of color at a regular spacing from there. The basis point floats over time, and once
+   * it moves far enough off the screen, it is brought back in range (to the next draw point).
+   */
+  private static void drawBGShimmer(Graphics g)
+  {
+    // Make a copy of the base polygon and translate it to the first draw point.
+    Polygon drawPoly = new Polygon(shimmerPoly.xpoints, shimmerPoly.ypoints, shimmerPoly.npoints);
+    int currentDrawPoint = (int)shimmerBasePoint; // Get the basis point to the nearest pixel.
+    drawPoly.translate(currentDrawPoint, 0);
+
+    // Draw all the shimmers.
+    g.setColor(BGSHIMMER);
+    for(; currentDrawPoint < dimensions.width; currentDrawPoint += shimmerSpacing)
+    {
+      // Draw the current shimmering band, then translate the polygon for the next.
+      g.fillPolygon(drawPoly);
+      drawPoly.translate(shimmerSpacing, 0);
+    }
+
+    // Time management.
+    long thisTime = System.currentTimeMillis();
+    long timeDiff = thisTime - lastDrawTime;
+    lastDrawTime = thisTime;
+
+    // Figure out how far to move the basis point.
+    // lastDrawTime only updates when this is called, so discard if too large.
+    double dist = timeDiff * shimmerPxPerMs;
+    dist = (dist > dimensions.width)? 0 : dist;
+
+    // Translate the basis point, bringing it back on-screen if needed.
+    shimmerBasePoint -= dist;
+    if(shimmerBasePoint < -(shimmerStretch+shimmerSpacing))
+    {
+      shimmerBasePoint += shimmerSpacing;
+    }
   }
 }
