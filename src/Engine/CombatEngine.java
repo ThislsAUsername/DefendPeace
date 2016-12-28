dc69dd78d9a6cac3eefb7f44e14af1e65a074c6c
ThatIsAUsername <ThatIsAUsername@users.noreply.github.com>
2016-12-28 12:28:56 -0600

    Added a way for COs to directly influence combat actions. Still need an example.

--- src/Engine/CombatEngine.java
+++ src/Engine/CombatEngine.java
@@ -11,19 +11,17 @@ public class CombatEngine
 
   public static void resolveCombat(Unit attacker, Unit defender, GameMap map)
   {
-    // TODO: make sure to clean up unneeded modifiers
-    //		for(int i = 0; i < modifiers.size(); i++) {
-    //			if (modifiers.get(i).done) {
-    //				modifiers.get(i).initTurn();
-    //				modifiers.remove(i);
-    //			}
-    //		}
-
     CombatParameters params = new CombatParameters(attacker, defender, map);
     for( int i = 0; i < modifiers.size(); i++ )
     {
       modifiers.get(i).alterCombat(params);
     }
+
+    // Last-minute adjustments.
+    attacker.CO.applyCombatModifiers(params);
+    defender.CO.applyCombatModifiers(params);
+
+    // Start bullets (shells, missiles, whatever) flying.
     params.defender.damageHP(params.calculateDamage());
     params.attacker.fire(params); // Lets the unit know that it has actually fired a shot.
     if( params.canCounter )
@@ -33,6 +31,9 @@ public class CombatEngine
       {
         modifiers.get(i).alterCombat(params);
       }
+
+      // TODO: Let the COs take another pass at modifying things?
+
       if( params.attackerHP > 0 ) // stops counterattacks from dead units unless a CombatModifier allows it
       {
         params.defender.damageHP(params.calculateDamage());
