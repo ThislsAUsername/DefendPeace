c37c8a2dba1812e758de329fd20f39afa1202e12
ThatIsAUsername <ThatIsAUsername@users.noreply.github.com>
2017-01-30 00:06:41 -0600

    Renamed GameEventSequence to GameEventQueue for clarity.

--- src/Engine/GameEvents/GameEventQueue.java
+++ src/Engine/GameEvents/GameEventQueue.java
@@ -2,7 +2,7 @@ package Engine.GameEvents;
 
 import java.util.ArrayDeque;
 
-public class GameEventSequence extends ArrayDeque<GameEvent>
+public class GameEventQueue extends ArrayDeque<GameEvent>
 {
   private static final long serialVersionUID = 2172691340188059948L;
 }
