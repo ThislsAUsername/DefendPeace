7ee125e5d225f300c70eef2784a6192f5050731b
Name <Visage@asu.edu>
2019-01-26 07:27:34 -0600

    Cleans out "extra" COs

--- src/CommandingOfficers/CommanderLibrary.java
+++ src/CommandingOfficers/CommanderLibrary.java
@@ -51,37 +51,37 @@ public class CommanderLibrary
     commanderList.add( BHSturm.getInfo() );
     commanderList.add( BHVB.getInfo() );
 
-    commanderList.add( BWBrennerCS.getInfo() );
+//    commanderList.add( BWBrennerCS.getInfo() );
     commanderList.add( BWBrennerComp.getInfo() );
-    commanderList.add( BWBrennerBasic.getInfo() );
+//    commanderList.add( BWBrennerBasic.getInfo() );
     commanderList.add( BWIsabellaCS.getInfo() );
-    commanderList.add( BWIsabellaBasic.getInfo() );
+//    commanderList.add( BWIsabellaBasic.getInfo() );
     commanderList.add( BWLinCS.getInfo() );
     commanderList.add( BWWillCS.getInfo() );
-    commanderList.add( BWWillBasic.getInfo() );
+//    commanderList.add( BWWillBasic.getInfo() );
 
     commanderList.add( LAForsythe.getInfo() );
     commanderList.add( LAGageCS.getInfo() );
-    commanderList.add( LAGageBasic.getInfo() );
+//    commanderList.add( LAGageBasic.getInfo() );
     commanderList.add( LATasha1star.getInfo() );
-    commanderList.add( LATashaLame.getInfo() );
+//    commanderList.add( LATashaLame.getInfo() );
     commanderList.add( LATasha40A.getInfo() );
-    commanderList.add( LATashaBasic.getInfo() );
+//    commanderList.add( LATashaBasic.getInfo() );
 
     commanderList.add( RAWaylonCS.getInfo() );
-    commanderList.add( RAWaylonBasic.getInfo() );
+//    commanderList.add( RAWaylonBasic.getInfo() );
 
     commanderList.add( IDSCaulderD2D.getInfo() );
     commanderList.add( IDSCaulderSCOP.getInfo() );
     commanderList.add( IDSPennyCS.getInfo() );
-    commanderList.add( IDSPennyRNG.getInfo() );
-    commanderList.add( IDSPennyBasic.getInfo() );
+//    commanderList.add( IDSPennyRNG.getInfo() );
+//    commanderList.add( IDSPennyBasic.getInfo() );
     commanderList.add( IDSTabithaCS.getInfo() );
-    commanderList.add( IDSTabithaBasic.getInfo() );
+//    commanderList.add( IDSTabithaBasic.getInfo() );
 
-    commanderList.add( LADavis.getInfo() );
+//    commanderList.add( LADavis.getInfo() );
     commanderList.add( TheBeastSturm.getInfo() );
-    commanderList.add( TheBeast.getInfo() );
+//    commanderList.add( TheBeast.getInfo() );
   }
 
   public static Commander makeCommander( CommanderInfo info, Color color, String faction )
