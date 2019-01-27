7fd9120c9e30e90b737160c75e4fe24116de843a
Name <Visage@asu.edu>
2019-01-27 17:08:34 -0600

    Vision modifier now takes types like moveMod does

--- src/CommandingOfficers/Modifiers/COVisionModifier.java
+++ src/CommandingOfficers/Modifiers/COVisionModifier.java
@@ -1,31 +1,49 @@
 package CommandingOfficers.Modifiers;
 
 import Units.UnitModel;
+
+import java.util.ArrayList;
+
 import CommandingOfficers.Commander;
 
 public class COVisionModifier implements COModifier
 {
+  ArrayList<UnitModel> modelsToModify;
   private int visionBoost = 0;
 
   public COVisionModifier(int boost)
   {
     visionBoost = boost;
+    modelsToModify = new ArrayList<UnitModel>();
   }
 
   @Override
   public void apply(Commander commander)
   {
-    for( UnitModel um : commander.unitModels )
+    for( UnitModel um : modelsToModify)
     {
       um.visionRange += visionBoost;
       um.visionIgnoresCover = true;
     }
   }
 
+  public void addApplicableUnitModel(UnitModel model)
+  {
+    if( model != null )
+    {
+      modelsToModify.add(model);
+    }
+    else
+    {
+      System.out.println("Attempting to add null model to COVisionModifier!");
+      throw new NullPointerException(); // Make sure this oversight doesn't go unnoticed.
+    }
+  }
+
   @Override
   public void revert(Commander commander)
   {
-    for( UnitModel um : commander.unitModels )
+    for( UnitModel um : modelsToModify)
     {
       um.visionRange -= visionBoost;
       um.visionIgnoresCover = false;
