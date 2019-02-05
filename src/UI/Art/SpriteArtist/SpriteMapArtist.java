731d5cfb4e3fffe84eeb9d3b707d0fe64e91462f
ThatIsAUsername <ThatIsAUsername@users.noreply.github.com>
2019-02-04 22:29:19 -0600

    SpriteMapArtist now only uses the blue move color if white fog is enabled.

--- src/UI/Art/SpriteArtist/SpriteMapArtist.java
+++ src/UI/Art/SpriteArtist/SpriteMapArtist.java
@@ -48,7 +48,7 @@ public class SpriteMapArtist
     // This nice little hack will semi-randomly decide what color fog is used for this game.
     // It switches every 10 seconds, so two games started 10 seconds apart will have different fog settings.
     String strTime = Long.toString(System.currentTimeMillis());
-    if( myGame.isFogEnabled() || (Integer.parseInt(new String() + (strTime.charAt(strTime.length() - 5))) % 2 == 0) )
+    if( !myGame.isFogEnabled() || (Integer.parseInt(new String() + (strTime.charAt(strTime.length() - 5))) % 2 == 0) )
     {
       FOG_COLOR = new Color(72, 72, 96, 200); // dark blue
       HIGHLIGHT_COLOR = new Color(255, 255, 255, 160); // white
