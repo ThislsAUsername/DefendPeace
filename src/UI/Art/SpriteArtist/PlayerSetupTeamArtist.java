6e7e9382f3603bb76c140794fc9fc87c343a70ed
ThatIsAUsername <ThatIsAUsername@users.noreply.github.com>
2019-06-02 01:01:00 -0500

    SpriteMiniMapArtist no longer gets confused by changing palettes.

--- src/UI/Art/SpriteArtist/PlayerSetupTeamArtist.java
+++ src/UI/Art/SpriteArtist/PlayerSetupTeamArtist.java
@@ -91,8 +91,7 @@ public class PlayerSetupTeamArtist
     {
       teamColors[i] = control.getPlayerInfo(i).getCurrentColor();
     }
-    SpriteMiniMapArtist.setTeamPalette(teamColors);
-    BufferedImage miniMap = SpriteMiniMapArtist.getMapImage( mapInfo );
+    BufferedImage miniMap = SpriteMiniMapArtist.getMapImage( mapInfo, teamColors );
 
     // Figure out how large to draw the minimap. We want to make it as large as possible, but still
     //   fit inside the available space (with a minimum scale factor of 1).
--- src/UI/Art/SpriteArtist/SpriteMiniMapArtist.java
+++ src/UI/Art/SpriteArtist/SpriteMiniMapArtist.java
@@ -3,7 +3,6 @@ package UI.Art.SpriteArtist;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.util.HashMap;
 
 import Engine.XYCoord;
 import Terrain.MapInfo;
@@ -11,31 +10,47 @@ import UI.UIUtils;
 
 public class SpriteMiniMapArtist
 {
-  private static HashMap<MapInfo, BufferedImage> mapImages = new HashMap<MapInfo, BufferedImage>();
-  private static Color[] mapPalette;
-  private static boolean mustRedraw;
+  private static MapInfo lastMapDrawn;
+  private static Color[] lastTeamColors;
+  private static BufferedImage lastMapImage;
 
   /**
    * Retrieve a BufferedImage with a 1-pixel-per-tile representation of the provided MapInfo.
    * Requested images are generated and store on first call, then simply fetched and returned
    * if this function is called again with the same MapInfo.
+   * Teams will be colored according to the default color ordering.
    */
   public static BufferedImage getMapImage(MapInfo mapInfo)
   {
-    if( mustRedraw || !mapImages.containsKey(mapInfo) )
+    return getMapImage(mapInfo, UIUtils.getCOColors());
+  }
+
+  /**
+   * Retrieve a BufferedImage with a 1-pixel-per-tile representation of the provided MapInfo.
+   * Requested images are generated and store on first call, then simply fetched and returned
+   * if this function is called again with the same MapInfo.
+   * @param teamColors A set of colors to be used for drawing each team. If insufficient colors
+   *        are provided, black will be used for any remaining ones. If null is passed in,
+   *        the default team-color ordering will be used.
+   */
+  public static BufferedImage getMapImage(MapInfo mapInfo, Color[] teamColors)
+  {
+    if( mapInfo != lastMapDrawn || palettesDiffer(teamColors) )
     {
+      System.out.println("Redrawing map.");
       // If we don't already have an image, generate and store it for later.
-      mapImages.put(mapInfo, generateMapImage(mapInfo));
-      mustRedraw = false;
+      lastMapImage = generateMapImage(mapInfo, teamColors);
+      lastMapDrawn = mapInfo;
+      lastTeamColors = teamColors;
     }
 
-    return mapImages.get(mapInfo);
+    return lastMapImage;
   }
 
   /**
    * Generates a BufferedImage with a 1-pixel-per=tile representation of the map.
    */
-  private static BufferedImage generateMapImage(MapInfo mapInfo)
+  private static BufferedImage generateMapImage(MapInfo mapInfo, Color[] teamColors)
   {
     BufferedImage image = new BufferedImage(mapInfo.getWidth(), mapInfo.getHeight(), BufferedImage.TYPE_INT_ARGB);
     Graphics g = image.getGraphics();
@@ -51,7 +66,7 @@ public class SpriteMiniMapArtist
     }
 
     // If we don't have a palette yet, just get the default one from UIUtis.
-    if( null == mapPalette ) mapPalette = UIUtils.getCOColors();
+    if( null == teamColors ) teamColors = UIUtils.getCOColors();
 
     // Draw team colors for properties that are owned at the start.
     for(int co = 0; co < mapInfo.COProperties.length; ++co)
@@ -59,7 +74,7 @@ public class SpriteMiniMapArtist
       Color coColor;
 
       // Log a warning if SpriteLibrary doesn't have enough colors to support this map.
-      if( co >= mapPalette.length )
+      if( co >= teamColors.length )
       {
         System.out.println("WARNING! '" + mapInfo.mapName + "' has more start locations than there are team colors!");
 
@@ -68,7 +83,7 @@ public class SpriteMiniMapArtist
       }
       else
       {
-        coColor = mapPalette[co];
+        coColor = teamColors[co];
       }
 
       // Loop through all locations assigned to this CO by mapInfo.
@@ -87,15 +102,29 @@ public class SpriteMiniMapArtist
     return image;
   }
 
-  /**
-   * Assign a set of colors to be used when generating a minimap image.
-   * @param teamColors An array of color values, one per player
-   * NOTE: If a palette is not set manually, UIUtils.getCOColors() will be used.
-   * NOTE: If too few colors are provided, black will be used as the fallback.
-   */
-  public static void setTeamPalette(Color[] teamColors)
+  private static boolean palettesDiffer(Color[] newColors)
   {
-    mapPalette = teamColors;
-    mustRedraw = true;
+    if( null == lastTeamColors || null == newColors )
+      return true;
+    if( lastTeamColors.length != newColors.length )
+      return true;
+
+    boolean colorsDiffer = false;
+    for( int i = 0; i < newColors.length; ++i )
+    {
+      if( colorsDiffer(newColors[i], lastTeamColors[i]) )
+      {
+        colorsDiffer = true;
+        break;
+      }
+    }
+
+    return colorsDiffer;
+  }
+
+  private static boolean colorsDiffer(Color c1, Color c2)
+  {
+    boolean same = (c1.getRed() == c2.getRed()) && (c1.getGreen() == c2.getGreen()) && (c1.getBlue() == c2.getBlue());
+    return !same;
   }
 }
