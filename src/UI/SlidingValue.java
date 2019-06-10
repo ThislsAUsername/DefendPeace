88a0758742c6878034e03806c7f2157045f8b5f7
ThatIsAUsername <ThatIsAUsername@users.noreply.github.com>
2019-06-09 22:48:11 -0500

    SlidingValue now works independently of framerate and call frequency of get().

--- src/UI/SlidingValue.java
+++ src/UI/SlidingValue.java
@@ -5,23 +5,30 @@ package UI;
  * Represents an XY-coordinate that slides quickly (but not instantly) into place when its location is set.
  * Allows users to see both the actual XY coordinate (if it did move instantly) as well as the intermediate
  * location that would be used for animating.
- * NOTE: It is currently expected for get() to be called at 60 frames per second for smooth animation.
  */
 public class SlidingValue
 {
+  private static final long UPDATE_DELAY_MS = 16; // Minimum time to update the intermediate position.
   private int actualValue;
   private double visualValue;
+  private long lastTime;
+  private long unusedTime;
 
   public SlidingValue(int val)
   {
     actualValue = val;
     visualValue = val;
+    lastTime = System.currentTimeMillis();
   }
 
   /** Set a target/destination for this SlidingPoint. */
   public void set(int val)
   {
-    actualValue = val;
+    if( val != actualValue )
+    {
+      actualValue = val;
+      lastTime = System.currentTimeMillis();
+    }
   }
 
   /** Retrieve the "current" location of the point as it slides to its destination. */
@@ -35,8 +42,8 @@ public class SlidingValue
     return visualValue;
   }
 
-  /** Retrieve the current destination ("real" position) of this sliding point. */
-  public int getActual()
+  /** Retrieve the current destination (the last set() value) of this sliding point. */
+  public int getDestination()
   {
     return actualValue;
   }
@@ -46,22 +53,31 @@ public class SlidingValue
    * just snapping instantly. Distance moved per frame is proportional to distance from goal location.
    * NOTE: This is calibrated for 60fps, and changing the frame rate will change the closure speed.
    */
-  private static double calculateSlideAmount(double currentNum, int targetNum)
+  private double calculateSlideAmount(double currentNum, int targetNum)
   {
-    double animMoveFraction = 0.3; // Movement cap to prevent over-speedy menu movement.
-    double animSnapDistance = 0.05; // Minimum distance at which point we just snap into place.
-    double slide = 0; // Return value; the distance we actually are going to move.
-    double diff = Math.abs(targetNum - currentNum);
-    int sign = (targetNum > currentNum) ? 1 : -1; // Since we took abs(), make sure we can correct the sign.
-    if( diff < animSnapDistance )
-    { // If we are close enough, just move the exact distance.
-      slide = diff;
-    }
-    else
-    { // Move a fixed fraction of the remaining distance.
-      slide = diff * animMoveFraction;
-    }
+    long time = System.currentTimeMillis();
+    long td = time - lastTime + unusedTime;
+    lastTime = time;
 
-    return slide * sign;
+    double slide = 0;
+    while( td > UPDATE_DELAY_MS )
+    {
+      td -= UPDATE_DELAY_MS;
+      double animMoveFraction = 0.3; // Movement amount per cycle.
+      double animSnapDistance = 0.05; // Minimum distance at which point we just snap into place.
+      double diff = Math.abs(targetNum - currentNum);
+      int sign = (targetNum > currentNum) ? 1 : -1; // Since we took abs(), make sure we can correct the sign.
+      if( diff < animSnapDistance )
+      { // If we are close enough, just move the exact distance.
+        slide = diff * sign;
+      }
+      else
+      { // Move a fixed fraction of the remaining distance.
+        slide += diff * animMoveFraction * sign;
+      }
+    }
+    unusedTime = td;
+
+    return slide;
   }
 }
