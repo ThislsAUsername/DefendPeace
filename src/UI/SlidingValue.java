67cbc62a93dca259d8168900ae300fbefe6f068b
ThislsAUsername <Visage@asu.edu>
2019-06-14 10:28:29 -0500

    Make yer slidin' snappy-like

--- src/UI/SlidingValue.java
+++ src/UI/SlidingValue.java
@@ -10,13 +10,16 @@ public class SlidingValue
 {
   private static final long UPDATE_DELAY_MS = 16; // Minimum time to update the intermediate position.
   private static double UPDATE_SCALE_FACTOR = 0.7; // What fraction of the difference we'll take up per UPDATE_DELAY
+  private static double ANIM_SNAP_DISTANCE = 0.5; // Minimum distance, at which point we just snap into place.
   private int startValue, endValue;
   private long startTime;
+  private boolean done;
 
   public SlidingValue(int val)
   {
     endValue = val;
     startValue = val;
+    done = false;
     startTime = System.currentTimeMillis();
   }
 
@@ -27,6 +30,7 @@ public class SlidingValue
     {
       startValue = (int) get();
       endValue = val;
+      done = false;
       startTime = System.currentTimeMillis();
     }
   }
@@ -36,14 +40,20 @@ public class SlidingValue
   {
     endValue = val;
     startValue = val;
+    done = true;
     startTime = System.currentTimeMillis();
   }
 
   /** Retrieve the "current" location of the point as it slides to its destination. */
   public double get()
   {
+    if (done)
+      return endValue;
     long td = System.currentTimeMillis() - startTime;
-    return endValue - (endValue - startValue) * Math.pow(UPDATE_SCALE_FACTOR, td / UPDATE_DELAY_MS);
+    double diff = (endValue - startValue) * Math.pow(UPDATE_SCALE_FACTOR, td / UPDATE_DELAY_MS);
+    if (Math.abs(diff) <= ANIM_SNAP_DISTANCE)
+      done = true;
+    return endValue - diff;
   }
 
   /** Retrieve the current destination (the last set() value) of this sliding point. */
