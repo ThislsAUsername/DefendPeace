d9632645c433bf5b0a5bd0062024efd3989480a7
ThatIsAUsername <ThatIsAUsername@users.noreply.github.com>
2020-04-17 11:41:46 -0500

    Visual tweaks per PR feedback.

--- src/UI/Art/SpriteArtist/ControlOptionsSetupArtist.java
+++ src/UI/Art/SpriteArtist/ControlOptionsSetupArtist.java
@@ -32,7 +32,7 @@ public class ControlOptionsSetupArtist
     myControl = control;
 
     // Set up some initial parameters.
-    int spacing = kbps.get(0).getImage().getHeight()*2;
+    int spacing = kbps.get(0).getKeysImage().getHeight()*2;
     int xDraw = spacing;
     int yDraw = spacing/2;
 
@@ -46,12 +46,26 @@ public class ControlOptionsSetupArtist
     BufferedImage controlsImage = SpriteLibrary.createTransparentSprite(dimensions.width/drawScale, dimensions.height/drawScale);
     Graphics cig = controlsImage.getGraphics();
 
-    // Loop through and draw everything, building or updating the necessary images.
+    // Loop through and draw the command names.
+    int maxCommandWidth = 0;
+    for( int ip = 0; ip < kbps.size(); ++ip )
+    {
+      KeyBindingPanel kbp = kbps.get(ip);
+      BufferedImage panel = kbp.getCommandImage();
+      cig.drawImage(panel, xDraw, yDraw, null);
+      if(panel.getWidth() > maxCommandWidth) maxCommandWidth = panel.getWidth();
+
+      yDraw += spacing;
+    }
+
+    // Loop through and draw all the bindings, building or updating the necessary images.
+    xDraw += maxCommandWidth + spacing/2; // Align keys right of the command column.
+    yDraw = spacing/2; // Reset y-spacing to draw keys.
     for( int ip = 0; ip < kbps.size(); ++ip )
     {
       KeyBindingPanel kbp = kbps.get(ip);
       boolean drawAltAdd = (ip == selectedAction) && myControl.isAssigningKey();
-      BufferedImage panel = kbp.getImage(drawAltAdd);
+      BufferedImage panel = kbp.getKeysImage(drawAltAdd);
       cig.drawImage(panel, xDraw, yDraw, null);
 
       if( ip == selectedAction ) // Draw the cursor over the selected item.
@@ -80,8 +94,10 @@ public class ControlOptionsSetupArtist
     // A couple of helper quantities.
     private static final int textBufferPx = 4;
     private static final int keyBufferPx = 8;
+    private static final boolean useSmallCaps = true;
 
-    // The composed TeamPanel image.
+    // The composed image.
+    private BufferedImage myCmdImage;
     private BufferedImage myImage;
 
     // Each frame that makes up the larger panel.
@@ -94,14 +110,28 @@ public class ControlOptionsSetupArtist
     public KeyBindingPanel(InputHandler.InputAction action)
     {
       myAction = action;
+
+      // Build an image representing the name of the command to which the keys are bound.
+      BufferedImage commandNamePane = SpriteUIUtils.getTextAsImage(myAction.name(), useSmallCaps);
+      int width = commandNamePane.getWidth() + textBufferPx;
+      int height = commandNamePane.getHeight() + textBufferPx;
+      commandNameFrame = new SpriteUIUtils.ImageFrame(0, 0, width, height,
+          SpriteUIUtils.MENUBGCOLOR, SpriteUIUtils.MENUFRAMECOLOR, false, commandNamePane);
+      myCmdImage = SpriteLibrary.createDefaultBlankSprite(width, height);
+      commandNameFrame.render(myCmdImage.getGraphics());
     }
 
-    public BufferedImage getImage()
+    public BufferedImage getCommandImage()
     {
-      return getImage(false);
+      return myCmdImage;
     }
 
-    public BufferedImage getImage(boolean addingKey)
+    public BufferedImage getKeysImage()
+    {
+      return getKeysImage(false);
+    }
+
+    public BufferedImage getKeysImage(boolean addingKey)
     {
       // If the number of bound keys hasn't changed, status quo remains (-1 for Add).
       if( (numFrames-1) == InputHandler.getBoundKeyCodes(myAction).size()
@@ -110,23 +140,15 @@ public class ControlOptionsSetupArtist
         return myImage;
       }
 
-      boolean notSmallCaps = true;
+      // Keep track of each pane's horizontal position.
       int xoffset = 0;
-      { // Build the first image, representing the name of the command to which the keys are bound.
-        BufferedImage commandNamePane = SpriteUIUtils.getTextAsImage(myAction.name(), notSmallCaps);
-        int width = commandNamePane.getWidth() + textBufferPx;
-        int height = commandNamePane.getHeight() + textBufferPx;
-        commandNameFrame = new SpriteUIUtils.ImageFrame(xoffset, 0, width, height,
-            SpriteUIUtils.MENUBGCOLOR, SpriteUIUtils.MENUFRAMECOLOR, true, commandNamePane);
-        xoffset += commandNameFrame.width + keyBufferPx;
-      }
 
       // Iterate over all bound keys and create images to display them as well.
       boundKeyFrames = new ArrayList<SpriteUIUtils.ImageFrame>();
       ArrayList<String> keysNames = InputHandler.getBoundKeyNames(myAction);
       for( String keyName : keysNames )
       {
-        BufferedImage keyPane = SpriteUIUtils.getTextAsImage(keyName, notSmallCaps);
+        BufferedImage keyPane = SpriteUIUtils.getTextAsImage(keyName, useSmallCaps);
         int paneWidth = keyPane.getWidth() + textBufferPx;
         int paneHeight = keyPane.getHeight() + textBufferPx;
 
@@ -140,7 +162,7 @@ public class ControlOptionsSetupArtist
 
       { // One more for the "Add" command.
         String addText = (addingKey) ? "..." : "Add";
-        BufferedImage addPane = SpriteUIUtils.getTextAsImage(addText, notSmallCaps);
+        BufferedImage addPane = SpriteUIUtils.getTextAsImage(addText, useSmallCaps);
         int addWidth = addPane.getWidth() + textBufferPx;
         int addHeight = addPane.getHeight() + textBufferPx;
         SpriteUIUtils.ImageFrame addFrame = new SpriteUIUtils.ImageFrame(xoffset, 0, addWidth, addHeight,
@@ -155,7 +177,6 @@ public class ControlOptionsSetupArtist
       // Re-render the panel.
       myImage = SpriteLibrary.createTransparentSprite( finalWidth, boundKeyFrames.get(0).height );
       Graphics g = myImage.getGraphics();
-      commandNameFrame.render(g);
       for( SpriteUIUtils.ImageFrame frame : boundKeyFrames )
       {
         frame.render(g);
