43f1b941131f32d19e4e8eaa83c212a015717dd9
ThatIsAUsername <ThatIsAUsername@users.noreply.github.com>
2020-05-29 10:57:51 -0500

    Added Tech with roughed-out TechDrop logic.

---
+++ src/CommandingOfficers/Tech.java
@@ -0,0 +1,230 @@
+package CommandingOfficers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Random;
+import java.util.Set;
+
+import AI.AIUtils;
+import CommandingOfficers.Modifiers.CODamageModifier;
+import CommandingOfficers.Modifiers.CODefenseModifier;
+import CommandingOfficers.Modifiers.COModifier;
+import Engine.GameScenario;
+import Engine.Utils;
+import Engine.XYCoord;
+import Engine.Combat.BattleSummary;
+import Engine.GameEvents.GameEventListener;
+import Terrain.Location;
+import Terrain.MapMaster;
+import Units.Unit;
+
+public class Tech extends Commander
+{
+  private static final long serialVersionUID = 1L;
+
+  private static final CommanderInfo coInfo = new instantiator();
+  private static class instantiator extends CommanderInfo
+  {
+    private static final long serialVersionUID = 1L;
+    public instantiator()
+    {
+      super("Tech");
+      infoPages.add(new InfoPage(
+          "Tech is a first-rate grease monkey who like nothing better than to create and deploy new technologies'\n"));
+      infoPages.add(new InfoPage(
+          "Passive:\n" +
+          "- Tech has some of the best mechanics around, allowing her units to repair 3 HP per turn instead of just 2.\n"));
+      infoPages.add(new InfoPage(
+          TechdropAbility.NAME + " (" + TechdropAbility.COST+  "):\n" +
+          "Gives an attack boost of " + TechdropAbility.BUFF + " to all units and deploys a Mechwarrior to the front lines.\n"));
+      infoPages.add(new InfoPage(
+          OVERCHARGE_NAME + " (" + OVERCHARGE_COST + "):\n" +
+          "Gives an attack boost of "+ OVERCHARGE_BUFF +
+          " and heals all units by " + OVERCHARGE_HEAL + ", allowing HP>10 for this turn.\n"));
+      infoPages.add(new InfoPage(
+          SPECIAL_DELIVERY_NAME + " (" + SPECIAL_DELIVERY_COST + "):\n" +
+          "Gives an attack boost of " + SPECIAL_DELIVERY_BUFF + " to all units and deploys three Mechwarriors to the front lines.\n"));
+    }
+    @Override
+    public Commander create(GameScenario.GameRules rules)
+    {
+      return new Tech(rules);
+    }
+  }
+
+  // Variables to characterize this Commander's abilities.
+  private static final int TECH_REPAIR = 3; // Used for both D2D heal and ability heal.
+
+  private static final String OVERCHARGE_NAME = "Overheal";
+  private static final int OVERCHARGE_COST = 6;
+  private static final int OVERCHARGE_BUFF = 20;
+  private static final int OVERCHARGE_HEAL = 3;
+
+  private static final String SPECIAL_DELIVERY_NAME = "Special Delivery";
+  private static final int SPECIAL_DELIVERY_COST = 9;
+  private static final int SPECIAL_DELIVERY_BUFF = 20;
+
+  public Tech(GameScenario.GameRules rules)
+  {
+    super(coInfo, rules);
+
+//    addCommanderAbility(new PatchAbility(this, AIRDROP_NAME, AIRDROP_COST, PLUNDER_INCOME, AIRDROP_BUFF));
+//    addCommanderAbility(new PatchAbility(this, OVERHEAL_NAME, OVERHEAL_COST, PILLAGE_INCOME, OVERHEAL_BUFF));
+
+  }
+
+  public static CommanderInfo getInfo()
+  {
+    return coInfo;
+  }
+
+  @Override
+  public int getRepairPower()
+  {
+    return TECH_REPAIR;
+  }
+
+  /** Drop a MechWarroir onto the most contested point on the battlefront. */
+  private static class TechdropAbility extends CommanderAbility
+  {
+    private static final long serialVersionUID = 1L;
+    private static final String NAME = "Airdrop";
+    private static final int COST = 3;
+    private static final int BUFF = 10;
+
+    private CODamageModifier damageBuff = null;
+    private CODefenseModifier defenseBuff = null;
+    private int dropRange;
+
+    TechdropAbility(Commander myCO, String abilityName, int abilityCost, int abilityRange)
+    {
+      super(myCO, abilityName, abilityCost);
+
+      // Create a COModifier that we can apply to Patch when needed.
+      damageBuff = new CODamageModifier(BUFF);
+      defenseBuff = new CODefenseModifier(BUFF);
+      dropRange = abilityRange;
+    }
+
+    @Override
+    protected void perform(MapMaster gameMap)
+    {
+      // Bump up our power level.
+      myCommander.addCOModifier(damageBuff);
+      myCommander.addCOModifier(defenseBuff);
+
+      // We want to find the spot where more muscle will do the most good; i.e. somewhere that is highly contested.
+      // Score spaces by subtracting the value of nearby friendlies from the value of nearby enemies.
+      // The result is how much cost-advantage the enemy has in that space.
+
+      // Start by computing friendly values. Note that only the spaces in this collection are eligible landing spaces.
+      Map<XYCoord, Integer> friendScores = new HashMap<XYCoord, Integer>();
+      for( XYCoord propCoord : myCommander.ownedProperties )
+      {
+        // Record any locations near owed properties; we want to be able to rescue unprotected structures.
+        for( XYCoord xyc : Utils.findLocationsInRange(gameMap, propCoord, dropRange) )
+          friendScores.put(xyc, 0);
+      }
+      for( Unit u : myCommander.units )
+      {
+        XYCoord uxy = new XYCoord(u.x, u.y);                       // Unit location
+        Integer uval = u.model.getCost() * u.getHP();              // Unit value
+        for( XYCoord xyc : Utils.findLocationsInRange(gameMap, uxy, dropRange) )
+        {
+          Integer curVal = friendScores.putIfAbsent(xyc, uval);      // Put value if absent
+          if( null != curVal ) friendScores.put(xyc, curVal + uval); // Combine values if already present
+        }
+
+        // Remove any spaces that already have friends in them - no smashing friends.
+        friendScores.remove(uxy);
+      }
+
+      // Next calculate unfriendly values. Note that these are only eligible landing spaces if they are also within
+      // range of friendly units, but it's easier to just compute all the values and then ignore invalid places.
+      ArrayList<XYCoord> enemyCoords = AIUtils.findEnemyUnits(myCommander, gameMap);
+      Map<XYCoord, Integer> enemyScores = new HashMap<XYCoord, Integer>();
+      for( XYCoord nmexy : enemyCoords )
+      {
+        Unit nme = gameMap.getLocation(nmexy).getResident();          // Enemy unit
+        Integer nmeval = nme.model.getCost() * nme.getHP();           // Enemy value
+
+        if(nmexy.getDistance(myCommander.HQLocation) < nme.model.movePower && nme.model.isTroop())
+          nmeval *= 12; // More weight if this unit threatens HQ.
+
+        for( XYCoord xyc : Utils.findLocationsInRange(gameMap, nmexy, dropRange) )
+        {
+          Integer curVal = enemyScores.putIfAbsent(xyc, nmeval);      // Put value if absent
+          if( null != curVal ) enemyScores.put(xyc, curVal + nmeval); // Combine values if already present
+        }
+
+        if( nme.model.isTroop() )
+          enemyScores.put(nmexy, enemyScores.get(nmexy) + nmeval);    // Bonus points for squashing infantry.
+        else enemyScores.remove(nmexy);                               // We can't squash larger things.
+      }
+
+      // Calculate and store scores in a pri-queue for easy sorting.
+      PriorityQueue<ScoredSpace> scoredSpaces = new PriorityQueue<ScoredSpace>();
+      for( XYCoord coord: friendScores.keySet() )
+      {
+        Integer fscore = friendScores.get(coord);
+        Integer escore = enemyScores.get(coord);
+        if( null == escore ) escore = 0;
+        scoredSpaces.add( new ScoredSpace(coord, escore-fscore) );
+      }
+
+      // If there are no good spaces... this should never happen.
+      if( scoredSpaces.isEmpty() )
+      {
+        System.out.println("[TechDrop.perform] Cannot find suitable landing zone. Aborting!");
+        return;
+      }
+
+      // Find all spaces that are equally contested.
+      ScoredSpace s1 = scoredSpaces.poll();
+      ArrayList<ScoredSpace> equalSpaces = new ArrayList<ScoredSpace>();
+      equalSpaces.add(s1);
+      while( !scoredSpaces.isEmpty() )
+      {
+        ScoredSpace ss = scoredSpaces.poll();
+        if( ss.score == s1.score )
+          equalSpaces.add(ss);
+        else break;
+      }
+
+      // Chose one at random if there are multiple equally-valid options.
+      int index = 0;
+      if( equalSpaces.size() > 1 )
+      {
+        Random rand = new Random(myCommander.units.size());
+        index = rand.nextInt(equalSpaces.size());
+      }
+      XYCoord landingZone = equalSpaces.get(index).space;
+
+      // EVENTS:
+      // | MechWarrior drops from the sky into place.
+      // | Victim on landing pad expires if present.
+    }
+
+    private class ScoredSpace implements Comparable<ScoredSpace>
+    {
+      XYCoord space;
+      int score;
+
+      public ScoredSpace(XYCoord space, int score)
+      {
+        this.space = space;
+        this.score = score;
+      }
+      
+      @Override
+      public int compareTo(ScoredSpace other)
+      {
+        return other.score - score;
+      }
+    }
+  }
+
+}
