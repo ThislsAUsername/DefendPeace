ca2d74d1224b3f533c3208574f75a00fd944b726
ThatIsAUsername <ThatIsAUsername@users.noreply.github.com>
2020-08-14 13:04:39 -0500

    HorizontalStreaksBG now uses Util.Random instead of Math.Random.

--- src/UI/Art/SpriteArtist/Backgrounds/HorizontalStreaksBG.java
+++ src/UI/Art/SpriteArtist/Backgrounds/HorizontalStreaksBG.java
@@ -1,13 +1,12 @@
 package UI.Art.SpriteArtist.Backgrounds;
 
+import java.util.Random;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Polygon;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
 
-import Engine.XYCoord;
 import UI.Art.SpriteArtist.SpriteLibrary;
 import UI.Art.SpriteArtist.SpriteOptions;
 
@@ -17,16 +16,17 @@ public class HorizontalStreaksBG
   private static int drawScale = SpriteOptions.getDrawScale();
   private static long lastDrawTime = System.currentTimeMillis(); // Used to control drift.
   private static double nextX = 0;
-  private static double movePxPerMs = 0.03; // Controls movement speed.
+  private static double movePxPerMs = 0.06; // Controls movement speed.
   private static int streakLengthMean = 48;
   private static int streakLengthStd = 10;
-  private static int streakWidthMean = 16;
-  private static int streakWidthStd = 5;
-  private static int streakAlphaMean = 190;
-  private static int streakAlphaStd = 20;
-  private static double saturation = 0.2;
+  private static int streakHeightMean = 12;
+  private static int streakHeightStd = 3;
+  private static double saturation = 0.3;
   private static Dimension lastDim;
   private static BufferedImage streaksImage;
+  private static int regenTolerance = 4;
+
+  private static Random random = new Random();
 
   public static void draw(Graphics g)
   {
@@ -43,7 +43,11 @@ public class HorizontalStreaksBG
     g.fillRect(0, 0, dimensions.width, dimensions.height);
 
     // If the scale has changed since we last checked, reinitialize.
-    if((!dimensions.equals(lastDim)) || (drawScale != SpriteOptions.getDrawScale()) || (null == streaksImage))
+    if( null == lastDim ||
+        Math.abs(dimensions.height - lastDim.height) > regenTolerance ||
+        Math.abs(dimensions.width - lastDim.width) > regenTolerance ||
+        drawScale != SpriteOptions.getDrawScale() ||
+        null == streaksImage)
     {
       lastDim = new Dimension(dimensions);
       drawScale = SpriteOptions.getDrawScale();
@@ -52,24 +56,23 @@ public class HorizontalStreaksBG
       // Generate an image with all of the streaks.
       streaksImage = SpriteLibrary.createTransparentSprite(dimensions.width, dimensions.height);
       Graphics sg = streaksImage.getGraphics();
-      sg.setColor(Color.WHITE);
-      sg.drawRect(0, 0, streaksImage.getWidth(), streaksImage.getHeight());
-      int numToGenerate = (int)(((dimensions.width*dimensions.height) / (streakWidthMean*streakLengthMean)) * saturation);
+      int numToGenerate = (int)(((dimensions.width*dimensions.height) / (streakHeightMean*streakLengthMean*drawScale*drawScale)) * saturation);
       for( int nn = 0; nn < numToGenerate; nn++ )
       {
-        double x = Math.random() * dimensions.width;
-        double y = Math.random() * dimensions.height;
-        int h = 32;
-        int l = 48;
-        Color c = new Color(255, 255, 255, (int)(Math.random() * 255));
+        int x = random.nextInt(dimensions.width);
+        int y = random.nextInt(dimensions.height);
+
+        int h = (int)(random.nextGaussian() * streakHeightStd + streakHeightMean) * drawScale;
+        int l = (int)(random.nextGaussian() * streakLengthStd + streakLengthMean) * drawScale;
+        Color c = new Color(255, 255, 255, random.nextInt(120) + 80);
 
         sg.setColor(c);
-        sg.fillRoundRect((int)x, (int)y, l, h, l/4, h/4);
+        sg.fillRoundRect(x, y, l, h, l/4, h/4);
 
         // If it's too close to the edge, redraw it on the other edge.
         if( x + l > dimensions.width )
         {
-          sg.fillRoundRect((int)(x-dimensions.width), (int)y, l, h, l/4, h/4);
+          sg.fillRoundRect(x-dimensions.width, y, l, h, l/4, h/4);
         }
       }
     }
