02a9411f4ed9ed26e3adecab6e3864ed41af20d2
ThislsAUsername <Visage@asu.edu>
2020-09-06 07:23:21 -0500

    Fix behavior for launched unit path selection

--- src/Engine/MapController.java
+++ src/Engine/MapController.java
@@ -385,11 +385,12 @@ public class MapController implements IController, GameInputHandler.StateChanged
     switch (inputType)
     {
       case CONSTRAINED_TILE_SELECT:
-      case PATH_SELECT:
       case FREE_TILE_SELECT:
         if( null != options && options.size() > 0 )
           myGame.setCursorLocation(options.get(0));
         break;
+      case PATH_SELECT:
+        break; // no special behavior
       case MENU_SELECT:
         Path path = myGameInputHandler.myStateData.path;
         if( null != path && path.getPathLength() > 0 )
