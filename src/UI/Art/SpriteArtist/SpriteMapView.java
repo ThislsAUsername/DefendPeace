49448f06c8de81bbff27669003bd6fe0d56af3dd
ThislsAUsername <ThislsAUsername@users.noreply.github.com>
2021-08-28 10:03:23 -0500

    Keep the turn number inside the renderable area (#343)
    * Keep the turn number inside the renderable area
    
    * Bottom-justify turn number text
    
    * Bottom-justify both 'Turn' and digit

--- src/UI/Art/SpriteArtist/SpriteMapView.java
+++ src/UI/Art/SpriteArtist/SpriteMapView.java
@@ -613,16 +613,26 @@ public class SpriteMapView extends MapView
     if( lastTurnNum != turnNum )
     {
       lastTurnNum = turnNum;
-      PixelFont pf = SpriteLibrary.getFontStandard();
-      BufferedImage day = SpriteUIUtils.getTextAsImage("Turn ");
-      BufferedImage dayNum = SpriteUIUtils.getBoldTextAsImage(Integer.toString(turnNum));
-      int width = day.getWidth() + dayNum.getWidth();
-      int height = Math.max(pf.getAscent(), dayNum.getHeight());
+      final PixelFont pf = SpriteLibrary.getFontStandard();
+      final int wordHeight = pf.getAscent()+pf.getDescent();
+
+      // Our final image will contain word+digit
+      final BufferedImage word = SpriteUIUtils.getTextAsImage("Turn ");
+      final BufferedImage digit = SpriteUIUtils.getBoldTextAsImage(Integer.toString(turnNum));
+
+      final int width = word.getWidth() + digit.getWidth();
+      final int height = Math.max(wordHeight, digit.getHeight());
 
       turnNumImage = SpriteLibrary.createTransparentSprite(width, height);
       Graphics dcg = turnNumImage.getGraphics();
-      dcg.drawImage(day, 0, 0, null);
-      dcg.drawImage(dayNum, turnNumImage.getWidth()-dayNum.getWidth(), pf.getAscent()-dayNum.getHeight(), null);
+
+      // Note that the word currently has no descender characters
+      int wordVOffset = height-wordHeight+pf.getDescent();
+      dcg.drawImage(word, 0, wordVOffset, null);
+
+      // Bottom-justify the digit, to match with the word
+      final int digitVOffset = height - digit.getHeight();
+      dcg.drawImage(digit, turnNumImage.getWidth()-digit.getWidth(), digitVOffset, null);
     }
 
     // Draw the turn counter.
