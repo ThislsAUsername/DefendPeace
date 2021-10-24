13b7f7fbda27a8e730322020060321797fcd1399
ThislsAUsername <Visage@asu.edu>
2021-10-23 19:00:02 -0500

    Rev 6:7:0

--- src/Engine/GameVersion.java
+++ src/Engine/GameVersion.java
@@ -13,7 +13,7 @@ public class GameVersion implements Serializable
   public GameVersion()
   {
     majorRev = 6; // For substantial, save-breaking changes to the game logic.
-    minorRev = 6; // For added content or UI updates
+    minorRev = 7; // For added content or UI updates
     hotfix   = 0; // For bugfixes (hopefully to never see increment)
   }
   
