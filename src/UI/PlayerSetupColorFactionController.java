d2577090158be2c390142cdceac5d841944bfc1d
ThislsAUsername <ThislsAUsername@users.noreply.github.com>
2022-10-16 22:57:30 -0500

    Enable picking multiple appearances for tags

--- src/UI/PlayerSetupColorFactionController.java
+++ src/UI/PlayerSetupColorFactionController.java
@@ -7,8 +7,11 @@ import UI.InputHandler.InputAction;
 public class PlayerSetupColorFactionController implements IController
 {
   private PlayerSetupInfo myPlayerInfo;
+  public final boolean shouldSelectMultiCO;
   OptionSelector colorSelector;
   OptionSelector factionSelector;
+  public int[] colorPicks, factionPicks;
+  public int tagIndex;
   public final String iconicUnitName;
 
   public PlayerSetupColorFactionController(PlayerSetupInfo playerInfo, String unitName)
@@ -16,11 +19,22 @@ public class PlayerSetupColorFactionController implements IController
     colorSelector = new OptionSelector(UIUtils.getCOColors().length);
     factionSelector = new OptionSelector(UIUtils.getFactions().length);
 
+    final int coCount = playerInfo.coList.size();
+    colorPicks   = new int[coCount];
+    factionPicks = new int[coCount];
+    for(int i = 0; i < coCount; ++i)
+    {
+      colorPicks[i]   = playerInfo.coList.get(i).color;
+      factionPicks[i] = playerInfo.coList.get(i).faction;
+    }
+
     // Start the selectors at the initial values.
     myPlayerInfo = playerInfo;
-    colorSelector.setSelectedOption(myPlayerInfo.coList.get(0).color);
-    factionSelector.setSelectedOption(myPlayerInfo.coList.get(0).faction);
-    
+    shouldSelectMultiCO = coCount > 1; // Don't need the TagMode since we only assign stuff for as many COs as we have
+    tagIndex = 0;
+    colorSelector.setSelectedOption(colorPicks[tagIndex]);
+    factionSelector.setSelectedOption(factionPicks[tagIndex]);
+
     iconicUnitName = unitName;
   }
 
@@ -31,10 +45,26 @@ public class PlayerSetupColorFactionController implements IController
     switch(action)
     {
       case SELECT:
-        // Apply change and return control.
-        myPlayerInfo.coList.get(0).color = colorSelector.getSelectionNormalized();
-        myPlayerInfo.coList.get(0).faction = factionSelector.getSelectionNormalized();
-        done = true;
+        colorPicks[tagIndex]   = colorSelector  .getSelectionNormalized();
+        factionPicks[tagIndex] = factionSelector.getSelectionNormalized();
+        ++tagIndex;
+        // If we have more tag COs, stick around and handle those...
+        if( tagIndex < colorPicks.length )
+        {
+          // Navigate the cursor to the current value of that index's position
+          // Is this good UX? Nobody knows!
+          colorSelector  .setSelectedOption(colorPicks  [tagIndex]);
+          factionSelector.setSelectedOption(factionPicks[tagIndex]);
+        }
+        else // Otherwise, apply change and return control.
+        {
+          for( int i = 0; i < colorPicks.length; ++i )
+          {
+            myPlayerInfo.coList.get(i).color   = colorPicks  [i];
+            myPlayerInfo.coList.get(i).faction = factionPicks[i];
+          }
+          done = true;
+        }
         break;
       case SEEK:
         // Flip units
@@ -49,8 +79,15 @@ public class PlayerSetupColorFactionController implements IController
         colorSelector.handleInput(action);
         break;
       case BACK:
-        // Don't apply changes; just return control.
-        done = true;
+        // Don't apply changes
+        --tagIndex;
+        if( tagIndex < 0 )
+          done = true;
+        else // If we're not backing out, point the cursor at the new-old selection
+        {
+          colorSelector  .setSelectedOption(colorPicks  [tagIndex]);
+          factionSelector.setSelectedOption(factionPicks[tagIndex]);
+        }
         break;
       default:
         // Do nothing.
