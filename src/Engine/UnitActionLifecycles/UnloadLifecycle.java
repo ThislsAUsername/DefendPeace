9986247d31580e644a5eb070c891e9c7cb35579c
ThislsAUsername <ThislsAUsername@users.noreply.github.com>
2023-06-01 22:54:45 -0700

    Allow failing to unload in fog

--- src/Engine/UnitActionLifecycles/UnloadLifecycle.java
+++ src/Engine/UnitActionLifecycles/UnloadLifecycle.java
@@ -3,6 +3,7 @@ package Engine.UnitActionLifecycles;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import Engine.GameAction;
 import Engine.GameActionSet;
@@ -18,7 +19,6 @@ import Terrain.MapMaster;
 import UI.MapView;
 import UI.Art.Animation.GameAnimation;
 import Units.Unit;
-import Units.UnitContext;
 
 public abstract class UnloadLifecycle
 {
@@ -133,15 +133,6 @@ public abstract class UnloadLifecycle
       if( isValid )
       {
         isValid &= !actor.heldUnits.isEmpty();
-        for( Unit cargo : myDropoffs.keySet() ) // Make sure the cargo can go where we want to put it.
-        {
-          isValid &= new UnitContext(cargo).calculateMoveType().canStandOn(gameMap.getEnvironment(myDropoffs.get(cargo)));
-        }
-        for( XYCoord coord : myDropoffs.values() ) // Make sure nobody's there already.
-        {
-          Unit res = gameMap.getLocation(coord).getResident();
-          isValid &= (null == res) || (res == actor); // Except the transport, because it must have moved anyway.
-        }
       }
 
       // Generate events.
@@ -150,13 +141,26 @@ public abstract class UnloadLifecycle
         // Attempt to move the transport to the target location.
         if( Utils.enqueueMoveEvent(gameMap, actor, movePath, unloadEvents) )
         {
-          // Debark the passengers. Unload all passengers you can, regardless of order.
-          for( Unit unit : myDropoffs.keySet() )
+          // Figure out what drops we can actually achieve
+          Map<Unit, XYCoord> successfulDrops = new HashMap<>();
+          for( Entry<Unit, XYCoord> drop : myDropoffs.entrySet() )
           {
-            XYCoord dropXY = myDropoffs.get(unit);
+            XYCoord dropXY = drop.getValue();
+            Unit res = gameMap.getLocation(dropXY).getResident();
+            // Make sure nobody's there already (except the transport, because it must have moved anyway)
+            if( (null == res) || (res == actor) )
+            {
+              successfulDrops.put(drop.getKey(), drop.getValue());
+            }
+          }
+
+          // Debark the passengers. Unload all passengers you can, regardless of order.
+          for( Entry<Unit, XYCoord> drop : successfulDrops.entrySet() )
+          {
+            XYCoord dropXY = drop.getValue();
             if( gameMap.isLocationEmpty(actor, dropXY) )
             {
-              unloadEvents.add(new UnloadEvent(actor, unit, myDropoffs.get(unit)));
+              unloadEvents.add(new UnloadEvent(actor, drop.getKey(), drop.getValue()));
             }
           }
         }
