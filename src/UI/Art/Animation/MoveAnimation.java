e26290adaf5cb2c58a629eece2109fe8ef8d91a8
ThislsAUsername <ThislsAUsername@users.noreply.github.com>
2023-07-20 05:59:51 -0700

    Base movement timing/duration on real movecosts

--- src/UI/Art/Animation/MoveAnimation.java
+++ src/UI/Art/Animation/MoveAnimation.java
@@ -7,28 +7,64 @@ import Engine.XYCoord;
 import Terrain.GameMap;
 import UI.Art.SpriteArtist.UnitSpriteSet.AnimState;
 import Units.Unit;
+import Units.UnitContext;
+import Units.MoveTypes.MoveType;
 
 public class MoveAnimation extends BaseUnitActionAnimation
 {
   private final long maxTime = 250;
 
   private final GamePath path;
-  private final double tilesPerMs;
+  private final double[] pathCosts; // in terms of milliseconds
 
   public MoveAnimation(int tileSize, GameMap map, Unit actor, GamePath path)
   {
     super(tileSize, actor, null);
     this.path = path;
-    tilesPerMs = actor.getMovePower(map) / (double) maxTime;
-    duration = (long) ((path.getPathLength() - 1) / tilesPerMs);
+
+    final UnitContext uc = new UnitContext(map, actor);
+    MoveType mt = uc.calculateMoveType();
+    final int tilesTraveled = path.getPathLength()-1;
+    pathCosts = new double[tilesTraveled];
+    double msPerCost = maxTime / (double) uc.calculateMovePower();
+    double msPerTeleport = maxTime / (double) tilesTraveled; // Teletiles should add slightly less than 1 tile's movement to the final duration
+
+    double msSpentTotal = 0;
+    for( int i = 0; i < tilesTraveled; ++i)
+    {
+      XYCoord destCoord  = path.getWaypoint( i+1 ).GetCoordinates();
+      int moveCost       = mt.getMoveCost(map.getEnvironment(destCoord));
+      double msSpent     = msPerCost * moveCost;
+      if( 0 == moveCost )
+        msSpent          = msPerTeleport;
+      msSpentTotal      += msSpent;
+      pathCosts[i]       = msSpent;
+    }
+    duration = (long) Math.ceil(msSpentTotal);
   }
 
   @Override
   public boolean animate(Graphics g)
   {
-    long animTime = System.currentTimeMillis() - startTime;
+    final long animTime = System.currentTimeMillis() - startTime;
 
-    final double tilesTraveled = animTime * tilesPerMs;
+    double tilesTraveled = 0;
+    double timeLeft = animTime;
+    int currentTile = 0;
+    while (0 < timeLeft && currentTile < pathCosts.length)
+    {
+      double thisTime = pathCosts[currentTile];
+      if( timeLeft >= thisTime )
+      {
+        timeLeft -= thisTime;
+        ++currentTile;
+        continue;
+      }
+      double fractionalMove = timeLeft / thisTime;
+      tilesTraveled = currentTile + fractionalMove;
+      break;
+    }
+
     final int prevTileIndex = Math.min((int) Math.floor(tilesTraveled), path.getPathLength() - 1);
     final int nextTileIndex = Math.min((int) Math.ceil (tilesTraveled), path.getPathLength() - 1);
 
