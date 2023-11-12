649720e07175ec381a73289aa1d794064d89a610
ThislsAUsername <ThislsAUsername@users.noreply.github.com>
2023-11-12 15:38:24 -0700

    Correct movement table definitions

--- src/Units/MoveTypes/FootStandard.java
+++ src/Units/MoveTypes/FootStandard.java
@@ -10,21 +10,13 @@ public class FootStandard extends MoveTypeLand
   public FootStandard()
   {
     // Make adjustments to the base-class values.
-    setMoveCost(Weathers.CLEAR, TerrainType.MOUNTAIN, 2);
-    setMoveCost(Weathers.CLEAR, TerrainType.RIVER, 2);
-
-    setMoveCost(Weathers.RAIN, TerrainType.DUNES, 3);
-    setMoveCost(Weathers.RAIN, TerrainType.MOUNTAIN, 3);
-    setMoveCost(Weathers.RAIN, TerrainType.RIVER, 2);
+    setMoveCost(TerrainType.MOUNTAIN, 2);
+    setMoveCost(TerrainType.RIVER, 2);
 
     setMoveCost(Weathers.SNOW, TerrainType.FOREST, 2);
     setMoveCost(Weathers.SNOW, TerrainType.GRASS, 2);
     setMoveCost(Weathers.SNOW, TerrainType.MOUNTAIN, 4);
-    setMoveCost(Weathers.SNOW, TerrainType.RIVER, 3);
 
     setMoveCost(Weathers.SANDSTORM, TerrainType.DUNES, 3);
-    setMoveCost(Weathers.SANDSTORM, TerrainType.MOUNTAIN, 2);
-    setMoveCost(Weathers.SANDSTORM, TerrainType.SHOAL, 2);
-    setMoveCost(Weathers.SANDSTORM, TerrainType.RIVER, 2);
   }
 }
--- src/Units/MoveTypes/Tread.java
+++ src/Units/MoveTypes/Tread.java
@@ -13,17 +13,12 @@ public class Tread extends MoveTypeLand
     setMoveCost(TerrainType.MOUNTAIN, 99);
     setMoveCost(TerrainType.RIVER, 99);
 
-    setMoveCost(Weathers.CLEAR, TerrainType.FOREST, 2);
-    setMoveCost(Weathers.CLEAR, TerrainType.DUNES, 2);
+    setMoveCost(TerrainType.FOREST, 2);
+    setMoveCost(TerrainType.DUNES, 2);
 
     setMoveCost(Weathers.RAIN, TerrainType.GRASS, 2);
     setMoveCost(Weathers.RAIN, TerrainType.FOREST, 3);
 
     setMoveCost(Weathers.SNOW, TerrainType.GRASS, 2);
-    setMoveCost(Weathers.SNOW, TerrainType.FOREST, 2);
-    setMoveCost(Weathers.SNOW, TerrainType.DUNES, 2);
-
-    setMoveCost(Weathers.SANDSTORM, TerrainType.FOREST, 2);
-    setMoveCost(Weathers.SANDSTORM, TerrainType.DUNES, 3);
   }
 }
