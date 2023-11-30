8a649a5d0f1e0526fea1cc8cc57c415fed41bda6
ThislsAUsername <ThislsAUsername@users.noreply.github.com>
2023-11-30 13:16:10 -0700

    JakeMan with COUs (#389)
    * Add basic COU logic to JakeMan
    
    * Add more precise comment text

---
+++ src/AI/COParser.java
@@ -0,0 +1,59 @@
+package AI;
+
+import java.util.ArrayList;
+
+import CommandingOfficers.Commander;
+import CommandingOfficers.DeployableCommander;
+import CommandingOfficers.RuinedCommander;
+import Engine.Army;
+import Units.Unit;
+
+/**
+ * Figures out which of your COs have a special sub-type you can glean more info from.
+ */
+public class COParser
+{
+  public final ArrayList<DeployableCommander> deployableCOs;
+  // Will necessarily be in the above list, but there may be more flavors later
+  public final ArrayList<RuinedCommander> dorCOs;
+
+  public COParser(Army army)
+  {
+    if( null == army )
+      throw new NullPointerException();
+
+    deployableCOs = new ArrayList<>();
+    for( Commander co : army.cos )
+    {
+      if( !(co instanceof DeployableCommander) )
+        continue;
+      deployableCOs.add((DeployableCommander) co);
+    }
+
+    dorCOs = new ArrayList<>();
+    for( Commander co : army.cos )
+    {
+      if( !(co instanceof RuinedCommander) )
+        continue;
+      dorCOs.add((RuinedCommander) co);
+    }
+  }
+
+  /**
+   * @return All COUs in your army
+   */
+  public ArrayList<Unit> getAllCOUs()
+  {
+    ArrayList<Unit> cous = new ArrayList<>();
+    for( DeployableCommander co : deployableCOs )
+    {
+      for( Unit u : co.COUs )
+        cous.add(u);
+      for( Unit u : co.COUsLost )
+        cous.remove(u);
+    }
+
+    return cous;
+  }
+
+}
