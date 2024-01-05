b3abbeb36fd8beba649e715a1506a06cbbf74488
ThislsAUsername <ThislsAUsername@users.noreply.github.com>
2024-01-05 09:47:50 -0700

    Implement DS Snow

--- src/Engine/GamePath.java
+++ src/Engine/GamePath.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import CommandingOfficers.Commander;
 import Terrain.GameMap;
+import Terrain.Environment.Weathers;
 import Units.Unit;
 import Units.UnitContext;
 import Units.UnitModel;
@@ -68,9 +69,25 @@ public class GamePath
     return getFuelCost(unit.CO, unit.model, map);
   }
   /**
-   * @return the amount of fuel it would cost to travel this path with the given unit type 
+   * @return the amount of fuel it would cost to travel this path with the given unit type
   **/
   public int getFuelCost(Commander co, UnitModel unit, GameMap map)
+  {
+    int fuelBurnRate = unit.fuelBurnPerTile;
+    PathNode start = waypoints.get(0);
+    if( map.getEnvironment(start.x, start.y).weatherType == Weathers.CHILL )
+      fuelBurnRate *= 2;
+    return getMoveCost(co, unit, map) * fuelBurnRate;
+  }
+
+  public int getMoveCost(Unit unit, GameMap map)
+  {
+    return getMoveCost(unit.CO, unit.model, map);
+  }
+  /**
+   * @return the amount of movepoints it would cost to travel this path with the given unit type
+  **/
+  public int getMoveCost(Commander co, UnitModel unit, GameMap map)
   {
     int cost = 0;
     MoveType fff = new UnitContext(co, unit).calculateMoveType();
@@ -81,7 +98,7 @@ public class GamePath
       XYCoord to   = waypoints.get( i ).GetCoordinates();
       cost += fff.getTransitionCost(map, from, to, co.army, true);
     }
-    return cost * unit.fuelBurnPerTile;
+    return cost;
   }
 
   public PathNode getWaypoint(int wpt)
