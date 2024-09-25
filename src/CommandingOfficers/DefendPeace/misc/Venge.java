872a773c7d1f1885c1d4d837152a4233be214acf
ThislsAUsername <ThislsAUsername@users.noreply.github.com>
2024-09-25 03:20:21 -0700

    Fix and rework Venge to be more interesting

--- src/CommandingOfficers/DefendPeace/misc/Venge.java
+++ src/CommandingOfficers/DefendPeace/misc/Venge.java
@@ -1,30 +1,36 @@
 package CommandingOfficers.DefendPeace.misc;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import CommandingOfficers.Commander;
 import CommandingOfficers.CommanderAbility;
 import CommandingOfficers.CommanderInfo;
 import Engine.Army;
 import Engine.GameScenario;
+import Engine.Utils;
+import Engine.XYCoord;
 import Engine.Combat.StrikeParams;
 import Engine.Combat.StrikeParams.BattleParams;
 import Engine.Combat.BattleSummary;
 import Engine.Combat.CombatContext;
+import Engine.Combat.DamagePopup;
 import Engine.GameEvents.GameEventQueue;
 import Engine.GameEvents.MassDamageEvent;
 import Engine.UnitActionLifecycles.JoinLifecycle.JoinEvent;
 import Engine.UnitMods.UnitDamageModifier;
 import Engine.UnitMods.UnitDefenseModifier;
 import Engine.UnitMods.UnitFightStatModifier;
-import Engine.UnitMods.UnitInstanceFilter;
 import Engine.UnitMods.UnitModifier;
 import Engine.UnitMods.UnitModifierWithDefaults;
+import Terrain.GameMap;
+import Terrain.MapLocation;
 import Terrain.MapMaster;
 import UI.UIUtils;
 import Units.Unit;
 import Units.UnitContext;
-import Units.UnitModel;
 
 /*
  * Venge enhances counter-attacks of all sorts.
@@ -45,20 +51,17 @@ public class Venge extends Commander
           "Attacking Venge is not always difficult, but you may not like the consequences.\n"));
       infoPages.add(new InfoPage(
           "Passive:\n" +
-          "- Baseline stats are lower than normal; -10 defense and attack.\n" +
-          "- After being attacked, Venge gets a bonus of +"+VENGEANCE_BOOST+"% attack against the unit that picked the fight.\n" +
-          "- Units that Venge can get vengeance on are marked with a V.\n"));
-      infoPages.add(new InfoPage(
-          IronWill.NAME+" ("+IronWill.COST+"):\n" +
-          "Only affects units that have not yet acted.\n" +
-          "Grants +"+IronWill.IRONWILL_BOOST+" offense and defense\n" +
-          "Your units resist damage and counterattack as if at full HP.\n" +
-          "When the power ends, your units lose "+IronWill.IRONWILL_WOUND+" health (nonlethal)\n"));
-      infoPages.add(new InfoPage(
-          Retribution.NAME+" ("+Retribution.COST+"):\n" +
-          "Gives an attack boost of +"+Retribution.RETRIBUTION_BUFF+"%\n" +
-          "Gives a defense penalty of +"+Retribution.RETRIBUTION_NERF+"%\n" +
-          "Units now counterattack before they are hit.\n"));
+          "-10 attack.\n" +
+          "Units that attack you or capture your property are marked for vengeance.\n" +
+          "Gain +"+VENGEANCE_BOOST+"% attack against marked units.\n"));
+      infoPages.add(new InfoPage(new Prevenge(null, null),
+          "Your units take their own revenge.\n" +
+          "Gives "+Prevenge.ATTACK+"/"+Prevenge.DEFENSE+" stats\n" +
+          "Your units counterattack before they are hit.\n"));
+      infoPages.add(new InfoPage(new Collectivenge(null, null),
+          "Collective punishment is the most effective punishment.\n" +
+          "Deal "+Collectivenge.HEALTH_DAMAGE+"% damage to enemies within radius "+Collectivenge.RADIUS+" of any marked unit.\n" +
+          "+10/10 stats\n"));
     }
     @Override
     public Commander create(GameScenario.GameRules rules)
@@ -66,10 +69,13 @@ public class Venge extends Commander
       return new Venge(rules);
     }
   }
+  public static CommanderInfo getInfo()
+  {
+    return coInfo;
+  }
 
-  /** A list of all the units that have attacked me since my last turn. */
+  /** Units I can take vengeance on. */
   private ArrayList<Unit> aggressors = new ArrayList<Unit>();
-  private IronWill myIronWill = new IronWill(this);
   /** How much power I get when beating them up */
   public final static int VENGEANCE_BOOST = 60;
 
@@ -77,21 +83,31 @@ public class Venge extends Commander
   {
     super(coInfo, rules);
 
-    addUnitModifier(new UnitFightStatModifier(-10));
-
-    addCommanderAbility(myIronWill);
-    addCommanderAbility(new Retribution(this, myIronWill.costBasis));
+    CommanderAbility.CostBasis cb = new CommanderAbility.CostBasis(CHARGERATIO_FUNDS);
+    addCommanderAbility(new Prevenge(this, cb));
+    addCommanderAbility(new Collectivenge(this, cb));
   }
 
-  public static CommanderInfo getInfo()
+  @Override
+  public void modifyUnitAttack(StrikeParams params)
   {
-    return coInfo;
+    params.attackPower -= 10;
+  }
+  @Override
+  public void modifyUnitAttackOnUnit(BattleParams params)
+  {
+    if( aggressors.contains(params.defender.unit) )
+    {
+      // Boost attack if it's time to avenge slights
+      params.attackPower += VENGEANCE_BOOST;
+    }
   }
 
   @Override
   public GameEventQueue receiveTurnEndEvent(Army army, int turn)
   {
-    aggressors.clear();
+    if( army == this.army )
+      aggressors.clear();
     return null;
   }
 
@@ -101,9 +117,6 @@ public class Venge extends Commander
     // If we can get a vengeance boost against this unit, let our player know.
     if( aggressors.contains(unit) )
       return 'V';
-    // If we ever allow COs other than our own to *activate* abilities, then this is gonna have to move to a StateTracker
-    if( myIronWill.boostedUnits.contains(unit) )
-      return 'I';
 
     return super.getUnitMarking(unit, activeArmy);
   }
@@ -113,6 +126,21 @@ public class Venge extends Commander
   {
     if (aggressors.contains(join.unitDonor))
       aggressors.add(join.unitRecipient);
+    return null;
+  }
+  @Override
+  public GameEventQueue receiveBattleEvent(BattleSummary battleInfo)
+  {
+    // Determine if we were attacked. If so, record this misdeed.
+    if( this == battleInfo.defender.CO )
+      aggressors.add(battleInfo.attacker.unit);
+    return super.receiveBattleEvent(battleInfo);
+  }
+  @Override
+  public GameEventQueue receiveCaptureEvent(Unit unit, Commander prevOwner, MapLocation location)
+  {
+    if( this == prevOwner )
+      aggressors.add(unit);
     return null;
   }
 
@@ -131,122 +159,21 @@ public class Venge extends Commander
     }
   }
 
-  public static class IronWillMod implements UnitModifierWithDefaults
+  private static class Prevenge extends CommanderAbility
   {
     private static final long serialVersionUID = 1L;
-
-    public final int buff;
-    public IronWillMod(int buff)
-    {
-      this.buff = buff;
-    }
-    @Override
-    public void modifyUnitAttack(StrikeParams params)
-    {
-      if( params.isCounter )
-      {
-        params.attackerHealth = UnitModel.MAXIMUM_HEALTH;
-      }
-      params.attackPower += buff;
-    }
-    @Override
-    public void modifyUnitDefenseAgainstUnit(BattleParams params)
-    {
-      params.defenderHealth = UnitModel.MAXIMUM_HEALTH;
-      params.defenseSubtraction += buff;
-    }
-  }
-
-  @Override
-  public void modifyUnitAttackOnUnit(BattleParams params)
-  {
-    if( aggressors.contains(params.defender.unit) )
-    {
-      // Boost attack if it's time to avenge slights
-      params.attackPower += VENGEANCE_BOOST;
-    }
-  }
-
-  @Override
-  public GameEventQueue receiveBattleEvent(BattleSummary battleInfo)
-  {
-    super.receiveBattleEvent(battleInfo);
-    // Determine if we were attacked. If so, record this misdeed.
-    if( this == battleInfo.defender.CO )
-    {
-      aggressors.add(battleInfo.attacker.unit);
-    }
-    return null;
-  }
-
-  private static class IronWill extends CommanderAbility
-  {
-    private static final long serialVersionUID = 1L;
-    private static final String NAME = "Iron Will";
-    private static final int COST = 4;
-    private static final int IRONWILL_BOOST = 40;
-    private static final int IRONWILL_WOUND = 20;
-    private final ArrayList<Unit> boostedUnits = new ArrayList<Unit>();
-
-    IronWill(Venge venge)
-    {
-      super(venge, NAME, COST);
-    }
-
-    @Override
-    public void enqueueUnitMods(MapMaster gameMap, ArrayList<UnitModifier> modList)
-    {
-      UnitInstanceFilter uif = new UnitInstanceFilter(new IronWillMod(IRONWILL_BOOST));
-      for( Unit unit : myCommander.army.getUnits() )
-      {
-        if( !unit.isTurnOver )
-          uif.instances.add(unit);
-      }
-      modList.add(uif);
-    }
-
-    @Override
-    protected void perform(MapMaster gameMap)
-    {
-      for( Unit unit : myCommander.army.getUnits() )
-      {
-        if( !unit.isTurnOver )
-          boostedUnits.add(unit);
-      }
-    }
-    @Override
-    protected void revert(MapMaster gameMap)
-    {
-      boostedUnits.clear();
-    }
-
-    @Override
-    public GameEventQueue getRevertEvents(MapMaster gameMap)
-    {
-      GameEventQueue events = new GameEventQueue();
-      events.add(new MassDamageEvent(myCommander, boostedUnits, IRONWILL_WOUND, false));
-      return events;
-    }
-  }
-
-  /**
-   * Retribution trades defense for firepower, and allows Venge to counter-attack first.
-   */
-  private static class Retribution extends CommanderAbility
-  {
-    private static final long serialVersionUID = 1L;
-    private static final String NAME = "Retribution";
-    private static final int COST = 8;
-    private static final int RETRIBUTION_BUFF = 40; // Trade defense for offense, since we hit before our attacker does.
-    private static final int RETRIBUTION_NERF = 20;
+    private static final String NAME = "Prevenge";
+    private static final int COST = 3;
+    private static final int ATTACK = 40; // Trade defense for offense, since we hit before our attacker does.
+    private static final int DEFENSE = -20;
     UnitModifier damageMod = null;
     UnitModifier defenseMod = null;
 
-    Retribution(Venge venge, CostBasis basis)
+    Prevenge(Venge venge, CostBasis basis)
     {
       super(venge, NAME, COST, basis);
-      damageMod = new UnitDamageModifier(RETRIBUTION_BUFF);
-      defenseMod = new UnitDefenseModifier(-RETRIBUTION_NERF);
+      damageMod  = new UnitDamageModifier(ATTACK);
+      defenseMod = new UnitDefenseModifier(DEFENSE);
     }
 
     @Override
@@ -255,6 +182,67 @@ public class Venge extends Commander
       modList.add(damageMod);
       modList.add(defenseMod);
       modList.add(new PreEmptiveCounterMod());
+    }
+  }
+
+  private static class Collectivenge extends CommanderAbility
+  {
+    private static final long serialVersionUID = 1L;
+    private static final String NAME = "Collectivenge";
+    UnitModifier statMod = new UnitFightStatModifier(10);
+    private static final int COST = 5;
+    private static final int HEALTH_DAMAGE = 60;
+    private static final int RADIUS = 2;
+    Venge coCast;
+
+    Collectivenge(Venge venge, CostBasis cb)
+    {
+      super(venge, NAME, COST, cb);
+      coCast = venge;
+    }
+
+    @Override
+    public void enqueueUnitMods(MapMaster gameMap, ArrayList<UnitModifier> modList)
+    {
+      modList.add(statMod);
+    }
+
+    @Override
+    public GameEventQueue getEvents(MapMaster gameMap)
+    {
+      GameEventQueue events = new GameEventQueue();
+      events.add(new MassDamageEvent(myCommander, findVictims(gameMap), HEALTH_DAMAGE, false));
+      return events;
+    }
+
+    @Override
+    public Collection<DamagePopup> getDamagePopups(GameMap gameMap)
+    {
+      ArrayList<DamagePopup> output = new ArrayList<DamagePopup>();
+
+      for( Unit victim : findVictims(gameMap) )
+      {
+        XYCoord coord = new XYCoord(victim);
+        output.add(new DamagePopup(
+                       coord,
+                       myCommander.myColor,
+                       Math.min(victim.getHealth()-1, HEALTH_DAMAGE) + "%"));
+      }
+
+      return output;
+    }
+
+    public HashSet<Unit> findVictims(GameMap gameMap)
+    {
+      Set<XYCoord> tilesInRange = Utils.findLocationsNearUnits(gameMap, coCast.aggressors, RADIUS);
+      HashSet<Unit> victims = new HashSet<Unit>(); // Find all of our unlucky participants
+      for( XYCoord coord : tilesInRange )
+      {
+        Unit victim = gameMap.getResident(coord);
+        if( null != victim && myCommander.isEnemy(victim.CO) )
+          victims.add(victim);
+      }
+      return victims;
     }
   }
   
