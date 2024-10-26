bf8be7e565fa27afb375f3940a5b453cd98da7b9
ThislsAUsername <ThislsAUsername@users.noreply.github.com>
2024-10-26 10:35:41 -0700

    Handle arbitrary new AWBW armies?

--- res/map/AWBWMapConverter.py
+++ res/map/AWBWMapConverter.py
@@ -172,7 +172,13 @@ def unitIDToString(x):
 		1141438: 'megatank',
 	}.get(x, 'oozium')
 
+neoPropCodes = [ 'AP', 'FC', 'CT', 'TW', 'HQ', 'LB', 'SP' ]
 def indexToTerrainCode(x):
+	if x >= 196: # start of AA (16); 203 starts NE (17)
+		neoProp = x - 196
+		neoArmy = (neoProp // 7) + 16 # integer division
+		neoProp = neoProp % 7
+		return f'{neoArmy}{neoPropCodes[neoProp]}'
 	return {
 		-1:  '  TT',#Teletiles
 		1:   '  GR',#plains/grass
@@ -343,20 +349,6 @@ def indexToTerrainCode(x):
 		193: '15LB',
 		194: '15SP',
 		195: '  TT',#Teletiles
-		196: '16AP',#AA
-		197: '16FC',
-		198: '16CT',
-		199: '16TW',
-		200: '16HQ',
-		201: '16LB',
-		202: '16SP',
-		203: '17AP',#NE
-		204: '17FC',
-		205: '17CT',
-		206: '17TW',
-		207: '17HQ',
-		208: '17LB',
-		209: '17SP',
 	}.get(x, '  XX')    # '  XX' is default if x not found
 
 
