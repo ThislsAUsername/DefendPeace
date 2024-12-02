9c5865d5297e9ce808f6f49893015a2001665c63
ThislsAUsername <ThislsAUsername@users.noreply.github.com>
2024-12-02 12:49:07 -0700

    Add some clarification text to SonjaDSBW and Stealth Greyfield

--- src/CommandingOfficers/AWBW/NRA/Greyfield.java
+++ src/CommandingOfficers/AWBW/NRA/Greyfield.java
@@ -36,6 +36,7 @@ public class Greyfield extends AWBWCommander
       super("Greyfield", UIUtils.SourceGames.AWBW, UIUtils.NRA, discriminator);
       infoPages.add(new InfoPage(
             "Greyfield (Sneak By Web)\n"
+          + "Alternative Greyfield port for the case that the Seaplane-converting one isn't accepted.\n"
           + "+10/40 stats for naval units, copters, and multirole attack planes (Stealths/Seaplanes)\n"));
       addSharedPages();
     }
--- src/CommandingOfficers/AWBW/YC/SonjaDSBW.java
+++ src/CommandingOfficers/AWBW/YC/SonjaDSBW.java
@@ -33,6 +33,8 @@ public class SonjaDSBW extends AWBWCommander
       super("Sonja", UIUtils.SourceGames.AWBW, UIUtils.YC, "3BW");
       infoPages.add(new InfoPage(
             "AWDS Sonja for AWBW\n"
+          + "DS Sonja is much more offense-focused than AW2's, and would likely add variety to T2.\n"
+          + "Also, this is a version of DS Sonja that doesn't mess with combat she isn't a party to.\n"
           + "+1 vision, -1 terrain star to enemies, 5 bad luck\n"
           + "Calculation order vs Lash SCOP: combat initiator applies first.\n"));
       infoPages.add(new InfoPage(new EnhancedVision(null, new CostBasis(CHARGERATIO_FUNDS)),
