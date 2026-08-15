9edd9ab2f5d797cd500152b9f41946dcbe5817f9
ThislsAUsername <ThislsAUsername@users.noreply.github.com>
2026-08-15 04:23:40 -0700

    Add leveling VB

--- src/CommandingOfficers/CommanderLibrary.java
+++ src/CommandingOfficers/CommanderLibrary.java
@@ -59,6 +59,7 @@ public class CommanderLibrary
     commanderList.add( CommandingOfficers.Variants.BH.SturmGrimmBW.getInfo() );
     commanderList.add( CommandingOfficers.Variants.BH.LashDSBW.getInfo() );
     commanderList.add( CommandingOfficers.Variants.BH.AdderD2D.getInfo() );
+    commanderList.add( CommandingOfficers.Variants.BH.VonBoltLevel.getInfo() );
     commanderList.add( Qis.getInfo() );
     commanderList.add( CommandingOfficers.AW4.BrennerWolves.Will.getInfo() );
     commanderList.add( CommandingOfficers.AW4.BrennerWolves.Lin.getInfo() );
